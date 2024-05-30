80c4ab4bcec57469c6244e66516ce6f0b2f81407
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-05-31 01:19:45 +0800

    added next.js app

--- bkg.js
+++
@@ -1,1 +0,0 @@
-new bkg = requires(fried-chicken)
--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("Hellow World!");
-console.log("Wow!");
-console.log("BKG was here.");
-console.log("fried chicken");
-
-
-//the speed is actually very fast!
